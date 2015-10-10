7c80496607826c0568b0bf5d8d98fdec62bc26e5
lauralilleste <lauralilleste@gmail.com>
2015-10-10 15:53:21 +0300

    Minor fix 2

--- test/get_nearest.js
+++ test/get_nearest.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('api', function() {
   describe('GET /nearest/?location=(58.3776730,26.7305030)&?categories=A,B&?bbox=(26.7100950,58.3744820,26.7518250,58.3812240)', function(req ,res) {
     it('Should respond with array of categories', function() {
-	  assert.equal(True, res instanceof Array);
+	  assert.equal(true, res instanceof Array);
       expect(200) 
     });
   });
--- test/get_objects.js
+++ test/get_objects.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('api', function() {
   describe('GET /objects/?location=(58.3776730,26.7305030)&?categories=A&?bbox=(26.7100950,58.3744820,26.7518250,58.3812240)', function(req ,res) {
     it('Should respond with array of objects', function() {
-	  assert.equal(True, res instanceof Array);
+	  assert.equal(true, res instanceof Array);
       expect(200) 
     });
   });
