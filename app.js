48ee0ce057fe4b9d465d56516922f50c79caddb7
Lauri Liivamägi <lauri@relion.info>
2015-10-17 20:40:29 +0300

    Use Hapi instead of Express. Install additonal dependency in Dockerfile.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var app = express();
+var Hapi = require('hapi');
+var server = new Hapi.Server();
 
-app.get('/', function (req, res) {
-  res.send('Hello from SpotScore API!');
+
+server.connection({ port: 3000 });
+
+server.start(function () {
+      console.log('Server running at:', server.info.uri);
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('SpotScore listening at http://%s:%s', host, port);
+server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+        reply('Hello from SpotScore API!');
+        }
 });
