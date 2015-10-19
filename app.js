19b6f0dadb1b1c6cfe400d984a0f600d1792f64a
Lauri Liivamägi <lauri@relion.info>
2015-10-19 20:40:20 +0300

    Add scaffolding for GET /objects route

--- app.js
+++ app.js
@@ -18,3 +18,30 @@ server.route({
         reply('Hello from SpotScore API!');
         }
 });
+
+server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+        reply('Hello from SpotScore API!');
+        }
+});
+
+server.route({
+    method: 'GET',
+    path: '/objects',
+    handler: function (request, reply) {
+        var location = request.params.location;
+        var address = request.params.address;
+        var categories = request.params.categories;
+        var bbox = request.params.bbox;
+        var radius = request.params.radius;
+        var nearest = request.params.nearest;
+
+        // Make actual request to database
+
+        // Compile the JSON response
+
+        reply();
+    }
+});
