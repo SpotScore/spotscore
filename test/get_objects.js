7b7a192884c7a419e85f48b34859599369f7e6a5
lauralilleste <lauralilleste@gmail.com>
2015-10-31 15:02:55 +0200

    removed braces

--- test/get_objects.js
+++ test/get_objects.js
@@ -6,7 +6,7 @@ var app = hapi();
 describe('GET /objects', function(){
   it('should return an array of objects inside the bbox"', function(done){
     request(app)
-      .get('objects/?location={58.377787,26.7251047}&?radius={30})
+      .get('objects/?location=58.377787,26.7251047&?radius=30)
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
