e3a48588fe782fad51bc1cfa447643348f5f0446
lauralilleste <lauralilleste@gmail.com>
2015-11-07 14:46:25 +0200

    Problem with address

--- test/get_objects.js
+++ test/get_objects.js
@@ -5,7 +5,7 @@ var request = require('supertest')
 describe('GET /objects', function(){
   it('should return an array of objects inside the bbox"', function(done){
     request(app)
-      .get('objects/?location=58.377787,26.7251047&?radius=30')
+      .get('/objects/?location=58.377787,26.7251047&?radius=30')
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
