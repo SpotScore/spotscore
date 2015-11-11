bd4e3d63f867c6eb443a155c6c353c0855be1053
Lauri Liivamägi <lauri@relion.info>
2015-11-12 01:09:11 +0200

    Use parameterized query. Other tweaking

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var pg = require('pg');
-var Hapi = require('hapi');
-var Good = require('good');
+"use strict";
 
-var conString = "postgres://spotscore:notsecret@localhost/gis";
+let pg = require('pg');
+let Hapi = require('hapi');
 
-var server = new Hapi.Server();
+let conString = "postgres://spotscore:notsecret@localhost/gis";
+
+let server = new Hapi.Server();
 
 server.connection({
     port: 3000
 });
-
 
 server.route({
     method: 'GET',
@@ -19,22 +19,27 @@ server.route({
     }
 });
 
-
 server.route({
     method: 'GET',
     path: '/objects',
     handler: function(request, reply) {
-        var lon = request.query.lon;
-        var lat = request.query.lat;
-        var address = request.query.address;
-        var categories = request.query.categories;
-        var bbox = request.query.bbox;
-        var radius = request.query.radius;
-        var nearest = request.query.nearest;
 
+        let lon = request.query.lon;
+        let lat = request.query.lat;
+        let address = request.query.address;
+        let categories = request.query.categories;
+        let bbox = request.query.bbox;
+        let radius = request.query.radius;
+        let nearest = request.query.nearest;
 
-        var query = "SELECT * FROM planet_osm_polygon pop WHERE pop.building = 'yes' AND ST_DWithin(Geography(ST_Transform(pop.way,4326)),ST_GeographyFromText('SRID=4326;POINT(" + lon + " " + lat + ")')," + radius + ");";
-
+        let template_query_1 =
+            `SELECT
+              *
+            FROM planet_osm_polygon pop
+            WHERE pop.building = 'yes'
+            AND
+            ST_DWithin(Geography(ST_Transform(pop.way,4326)),
+            ST_GeographyFromText('SRID=4326;POINT(${lon} ${lat})'), $1);`
 
         //this initializes a connection pool
         //it will keep idle connections open for a (configurable) 30 seconds
@@ -43,15 +48,16 @@ server.route({
             if (err) {
                 return console.error('error fetching client from pool', err);
             }
-            client.query(query, function(err, result) {
-                //call `done()` to release the client back to the pool
-                done();
+            client.query({name:"prepared_1", text:template_query_1, values:[radius]},
+                function(err, result) {
+                    //call `done()` to release the client back to the pool
+                    done();
 
-                if (err) {
-                    return console.error('error running query', err);
-                }
-                reply(result).type('application/json');
-            });
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
+                    reply(result).type('application/json');
+                });
         });
 
     }
@@ -62,5 +68,4 @@ server.start(function() {
     console.log('Server running at:', server.info.uri);
 });
 
-
-exports.server = server;
+module.exports.server = server;
