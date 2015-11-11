51c741b50447ed46eb8d5d3451dd9868d21a7f0e
Lauri Liivamägi <lauri@relion.info>
2015-11-12 01:37:07 +0200

    Just wondering around and updating ESLint rules

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let pg = require('pg');
-let Hapi = require('hapi');
+let pg = require("pg");
+let Hapi = require("hapi");
 
 let conString = "postgres://spotscore:notsecret@localhost/gis";
 
@@ -12,25 +12,25 @@ server.connection({
 });
 
 server.route({
-    method: 'GET',
-    path: '/',
+    method: "GET",
+    path: "/",
     handler: function(request, reply) {
-        reply('Hello from SpotScore API!');
+        reply("Hello from SpotScore API!");
     }
 });
 
 server.route({
-    method: 'GET',
-    path: '/objects',
+    method: "GET",
+    path: "/objects",
     handler: function(request, reply) {
 
         let lon = request.query.lon;
         let lat = request.query.lat;
-        let address = request.query.address;
-        let categories = request.query.categories;
-        let bbox = request.query.bbox;
+        // let address = request.query.address;
+        // let categories = request.query.categories;
+        // let bbox = request.query.bbox;
         let radius = request.query.radius;
-        let nearest = request.query.nearest;
+        // let nearest = request.query.nearest;
 
         let template_query_1 =
             `SELECT
@@ -39,14 +39,14 @@ server.route({
             WHERE pop.building = 'yes'
             AND
             ST_DWithin(Geography(ST_Transform(pop.way,4326)),
-            ST_GeographyFromText('SRID=4326;POINT(${lon} ${lat})'), $1);`
+            ST_GeographyFromText('SRID=4326;POINT(${lon} ${lat})'), $1);`;
 
         //this initializes a connection pool
         //it will keep idle connections open for a (configurable) 30 seconds
         //and set a limit of 20 (also configurable)
         pg.connect(conString, function(err, client, done) {
             if (err) {
-                return console.error('error fetching client from pool', err);
+                return console.error("error fetching client from pool", err);
             }
             client.query({name:"prepared_1", text:template_query_1, values:[radius]},
                 function(err, result) {
@@ -54,9 +54,9 @@ server.route({
                     done();
 
                     if (err) {
-                        return console.error('error running query', err);
+                        return console.error("error running query", err);
                     }
-                    reply(result).type('application/json');
+                    reply(result).type("application/json");
                 });
         });
 
@@ -65,7 +65,7 @@ server.route({
 
 
 server.start(function() {
-    console.log('Server running at:', server.info.uri);
+    console.log("Server running at:", server.info.uri);
 });
 
 module.exports.server = server;
