44ce4da39bff3d036d529a3d68a5fe9ecebb826e
lauralilleste <lauralilleste@gmail.com>
2015-12-06 20:56:31 +0200

    res length

--- test/objects2.js
+++ test/objects2.js
@@ -9,7 +9,7 @@ lab.experiment('GET /objects', { timeout: 3000 }, function () {
       server.inject({ method: 'GET', url: '/objects?lat=58.377787&lon=26.7251047&radius=10'}, function (res) {
 		  res.statusCode.should.equal(200);
           res.headers['content-type'].should.equal('application/json; charset=utf-8');
-		  return res.toString();
+		  res.should.not.have.length(0);
           done();
         });
   });
