949276a5e54ee76e835a6f838c015c6999bab392
lauralilleste <lauralilleste@gmail.com>
2015-12-06 20:38:03 +0200

    return res

--- test/objects2.js
+++ test/objects2.js
@@ -7,9 +7,9 @@ var server = require("../app.js").server,
 lab.experiment('GET /objects', { timeout: 3000 }, function () {
   lab.test('response status code should equal 200 and contain utf-8 JSON', function (done) {
       server.inject({ method: 'GET', url: '/objects?lat=58.377787&lon=26.7251047&radius=30'}, function (res) {
-		  res.should.have.length(1);
 		  res.statusCode.should.equal(200);
           res.headers['content-type'].should.equal('application/json; charset=utf-8');
+		  return res.toString();
           done();
         });
   });
