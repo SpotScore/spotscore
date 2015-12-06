c8d36aa09d6d4943cedaebc50a1537c7e9b7b57b
lauralilleste <lauralilleste@gmail.com>
2015-12-06 20:20:22 +0200

    length test alteration2

--- test/objects2.js
+++ test/objects2.js
@@ -7,8 +7,7 @@ var server = require("../app.js").server,
 lab.experiment('GET /objects', { timeout: 3000 }, function () {
   lab.test('response status code should equal 200 and contain utf-8 JSON', function (done) {
       server.inject({ method: 'GET', url: '/objects?lat=58.377787&lon=26.7251047&radius=30'}, function (res) {
-          var result = res.result;
-		  result.should.have.length(1);
+		  res.should.have.length(1);
 		  res.statusCode.should.equal(200);
           res.headers['content-type'].should.equal('application/json; charset=utf-8');
           done();
