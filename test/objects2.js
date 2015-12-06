0bc27f5173220e54179c817d39384a39ecff9d4c
lauralilleste <lauralilleste@gmail.com>
2015-12-06 20:41:33 +0200

    return res2

--- test/objects2.js
+++ test/objects2.js
@@ -6,7 +6,7 @@ var server = require("../app.js").server,
 
 lab.experiment('GET /objects', { timeout: 3000 }, function () {
   lab.test('response status code should equal 200 and contain utf-8 JSON', function (done) {
-      server.inject({ method: 'GET', url: '/objects?lat=58.377787&lon=26.7251047&radius=30'}, function (res) {
+      server.inject({ method: 'GET', url: '/objects?lat=58.377787&lon=26.7251047&radius=10'}, function (res) {
 		  res.statusCode.should.equal(200);
           res.headers['content-type'].should.equal('application/json; charset=utf-8');
 		  return res.toString();
