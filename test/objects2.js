5734469d0ca1d2294e45bb3cdd4972257075bbd5
lauralilleste <lauralilleste@gmail.com>
2015-12-06 21:10:42 +0200

    length 4

--- test/objects2.js
+++ test/objects2.js
@@ -9,7 +9,7 @@ lab.experiment('GET /objects', { timeout: 3000 }, function () {
       server.inject({ method: 'GET', url: '/objects?lat=58.377787&lon=26.7251047&radius=10'}, function (res) {
 		  res.statusCode.should.equal(200);
           res.headers['content-type'].should.equal('application/json; charset=utf-8');
-		  res.raw['length'].should.not.equal(0);
+		  res.raw.should.not.have.length(0);
           done();
         });
   });
